8f0b60a7605915b180c517b05b946ffa4338eec6
cjackson7-svg <cjackson7@mymail.tstc.edu>
2025-11-04 18:21:54 -0600

    Update arraysTryCatch.js
    added more comments

--- 25SP-ITSE2302-C-Jackson-Module4/25SP-ITSE2302-C-Jackson-Module4/25SP-ITSE2302-C-Jackson-Module3/25SP-ITSE2302-C-Jackson-Module2/Module 1 Mastery/Javascript/arraysTryCatch.js
+++ 25SP-ITSE2302-C-Jackson-Module4/25SP-ITSE2302-C-Jackson-Module4/25SP-ITSE2302-C-Jackson-Module3/25SP-ITSE2302-C-Jackson-Module2/Module 1 Mastery/Javascript/arraysTryCatch.js
@@ -4,19 +4,19 @@ var dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturda
 var firstName = "Chamoa"
 var birthyear = 1992;
 
-siteTopics.push("Mindset");
+siteTopics.push("Mindset"); // Add a new topic to the end of the array
 
 console.log("The legnth of the Site Topics array is " + siteTopics.length);
 
-var i =0;
-for (i = 0; i < siteTopics.length; i++) {
-    console.log( siteTopics[i]);
+var i =0; // Initialize counter variable
+for (i = 0; i < siteTopics.length; i++) { // Loop through each element in the array
+    console.log( siteTopics[i]);    
 }
 
-dayNames.reverse();
-console.log("The reversed Day Names array is: " + dayNames);
-dayNames.shift();
-console.log("The Day Names array after shift is: " + dayNames);
+dayNames.reverse(); // Reverse the order of the array
+console.log("The reversed Day Names array is: " + dayNames); // Log the reversed array
+dayNames.shift(); // Remove the first element from the array
+console.log("The Day Names array after shift is: " + dayNames); // Log the array after removing the first element
 
 //try catch block
 try {
@@ -32,9 +32,9 @@ try {
 
 //finally block
 try {
-    var Name = (firstName.toUpperCase) + (birthyear)();
+    var Name = (firstName.toUpperCase) + (birthyear)(); // This will cause an error
 } catch (error) {
-    console.error("An error occurred: " + error.message);       
-} finally {
+    console.error("An error occurred: " + error.message);     // Log the error message  
+} finally { // This block will always execute
     console.log(firstName + " was born in the year " + birthyear + ". I think the day was either a " + dayNames[1] + " or " + dayNames[3] + ".");
 }
